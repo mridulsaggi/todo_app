a654f225b5dd08ae52da69256646ac386835302d
mridul saggi <91930535+mridulsaggi@users.noreply.github.com>
2023-08-04 20:09:44 -0700

    updated changes

--- functions/user/getmydetails.js
+++ functions/user/getmydetails.js
@@ -2,10 +2,11 @@ import express from "express"
 import jwt from "jsonwebtoken"
 import { user } from "../../utils/schema.js";
 import cp from "cookie-parser";
-import { isauth } from "../isauth.js";
 const app=express();
 app.use(cp());
 export const getmydetails=async(req,res)=>{
+    const {name}=req.user
+    console.log(name);
     res.json({
         sucess:true,
         message:"user found",
